15f83a766ff49e262f1d46d6fd510ab6eaa9c0dc
ozh <ozh@ozh.org>
2013-04-08 22:50:21 +0200

    First commit

---
+++ plugin.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Your Plugin Name
+Plugin URI: http://github.com/your_name/your_plugin
+Description: One line description of your plugin
+Version: 1.0
+Author: Your Name
+Author URI: http://your-site-if-any/
+*/
+
+// No direct call
+if( !defined( 'YOURLS_ABSPATH' ) ) die();
+
+/*
+
+ Your code goes here.
+ 
+ Suggested read:
+ https://github.com/YOURLS/YOURLS/wiki/Coding-Standards
+ https://github.com/YOURLS/YOURLS/wiki#for-developpers
+ https://github.com/YOURLS/YOURLS/wiki/Plugin-List#get-your-plugin-listed-here
+ 
+ Have fun!
+ 
+*/
